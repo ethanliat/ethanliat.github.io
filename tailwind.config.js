b6e2c8a39e6626158437f37802ece92107925043
Ethan McCosky <ethanliat@gmail.com>
2023-08-05 12:05:15 -0500

    Update css and IG Link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
+  content: ["./docs/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
     colors: {
       "brand-yellow": "#d3c04c",
