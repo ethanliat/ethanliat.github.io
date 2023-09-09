b8f07e97e965d3721f4fe99f37da960d5d3f03d1
Ethan McCosky <emccosky@yipitdata.com>
2023-09-09 11:32:48 -0500

    update homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,17 @@
 module.exports = {
   content: ["./docs/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
-    colors: {
-      "brand-yellow": "#d3c04c",
-      "brand-teal": "#6fc2b8",
-      "brand-red": "#6fc2b8",
-      "brand-purple": "#6fc2b8",
-    },
     fontFamily: {
       sans: ["Graphik", "sans-serif"],
       serif: ["Merriweather", "serif"],
     },
     extend: {
+      colors: {
+        "brand-yellow": "#d3c04c",
+        "brand-teal": "#6fc2b8",
+        "brand-red": "#6fc2b8",
+        "brand-purple": "#6fc2b8",
+      },
       spacing: {
         "8xl": "96rem",
         "9xl": "128rem",
@@ -23,5 +23,5 @@ module.exports = {
     },
   },
   plugins: [require("tw-elements/dist/plugin.cjs")],
-  darkMode: "class"
+  darkMode: "class",
 };
